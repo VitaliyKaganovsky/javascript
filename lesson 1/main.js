2ae5558612c693d850d51ebe21ea07ef7f9e0c72
VitaliyKaganovskyy <kvs1996@gmail.com>
2022-07-21 22:26:41 +0300

    homework

--- lesson 1/main.js
+++ lesson 1/main.js
@@ -1,6 +1,70 @@
-let a = `22`;
-console.log(a);
+// task 1
 
-let b = a;
+// let a = `hello`;
+// let b = `owu`;
+// let c = `com`;
+// let d = `ua`;
+// let e = 1;
+// let f = 10;
+// let g = -999;
+// let h = 123;
+// let i = 3.14;
+// let j = 2.7;
+// let k = 16;
+// let l = true;
+// let m = false;
+//
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+// console.log(e);
+// console.log(f);
+// console.log(g);
+// console.log(h);
+// console.log(i);
+// console.log(j);
+// console.log(k);
+// console.log(l);
+// console.log(m);
 
-console.log(b);
+// alert(a);
+// alert(b);
+// alert(c);
+// alert(d);
+// alert(e);
+// alert(f);
+// alert(g);
+// alert(h);
+// alert(i);
+// alert(j);
+// alert(k);
+// alert(l);
+// alert(m);
+
+// task 2
+
+let firstName = `Vitaliy`;
+let middleName = `sir:D`;
+let lastName = `Kaganovskyy`;
+
+let person = (firstName + ` ` + middleName + ` ` + lastName);
+console.log(person);
+
+// task 3
+
+let a = 100;
+let b = '100';
+let c = true;
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+
+// task 4
+
+let name = prompt(`Введіть Ваше імя`);
+let mName = prompt(`Введіть ваше по-батькові`);
+let age = prompt('Скільки Вам років?');
+
+console.log(`Мене звати ` + name + ` ` + mName + ` і мені ` + age + ` років`);
