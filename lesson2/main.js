e93c26e62147c8c1eec7b71a4770d6a61c9ad5ce
VitaliyKaganovskyy <kvs1996@gmail.com>
2022-07-27 10:06:56 +0300

    js hw lesson 2 added

---
+++ lesson2/main.js
@@ -0,0 +1,152 @@
+// task 1
+
+// let array = [21,`volvo`, false, 3.14, `name`, [22, `member`], `task`, -1, `phone`, 43.54]
+//
+// console.log(array);
+
+// task 2
+
+// let HPotter = {
+//     title: `Harry Potter`,
+//     pageCount: 326,
+//     genre: `fantasy`
+// };
+//
+// console.log(HPotter);
+//
+//
+// let GMile = {
+//     title: `The Green Mile`,
+//     pageCount: 223,
+//     genre: `horror`
+// }
+//
+// console.log(GMile);
+//
+//
+// let Or1984 = {
+//     title: `1984`,
+//     pageCount: 104,
+//     genre: `Science fiction`
+// }
+//
+// console.log(Or1984);
+
+//
+// task 3
+//
+// let book1 = {
+//     title: `Depeche Mode`,
+//     pageCount: 87,
+//     genre: `Fiction`,
+//     authors: [
+//         {
+//             name: `Serhii Zhadan`,
+//             age: 31
+//         },
+//         {
+//             name: `One More Author`,
+//             age: 28
+//         }
+//     ]
+// }
+//
+// console.log(book1);
+//
+//
+// let book2 = {
+//     title: `Be Happy`,
+//     pageCount: 90,
+//     genre: `Basic`,
+//     authors: [
+//         {
+//             name: `Luiza L Hey`,
+//             age: 68
+//         },
+//         {
+//             name: `One More Author`,
+//             age: 34
+//         }
+//     ]
+// }
+//
+// console.log(book2);
+//
+//
+// let book3 = {
+//     title: `Martin Iden`,
+//     pageCount: 192,
+//     genre: `Fiction`,
+//     authors:[
+//         {
+//             name: `Jack London`,
+//             age: 67
+//         },
+//         {
+//             name: `One More Author`,
+//             age: 72
+//         }
+//     ]
+// }
+//
+// console.log(book3);
+
+// task 4
+
+// let users = [
+//     {
+//       name: `Vitaliy`,
+//       username: `vitaliy96`,
+//       password: `asdf235512`
+//     },
+//     {
+//         name: `Andriy`,
+//         username: `andriy228`,
+//         password: `daksd44`
+//     },
+//     {
+//         name: `Viktor`,
+//         username: `viktor93`,
+//         password: `fsjdfsf445`
+//     },
+//     {
+//         name: `Petro`,
+//         username: `petro81`,
+//         password: `parolparol`
+//     },
+//     {
+//         name: `Oleg`,
+//         username: `oleg87`,
+//         password: `nevzlamaesh14`
+//     },
+//     {
+//         name: `Grigoriy`,
+//         username: `grisha90`,
+//         password: `hackerman28`
+//     },
+//     {
+//         name: `Volodymyr`,
+//         username: `vovan`,
+//         password: `uftuftuft`
+//     },
+//     {
+//         name: `Rostyslav`,
+//         username: `rostik22`,
+//         password: `qwertyytrewq`
+//     },
+//     {
+//         name: `Maryana`,
+//         username: `maryana`,
+//         password: `100021002`
+//     },
+//     {
+//         name: `Maksym`,
+//         username: `maks100500`,
+//         password: `boobooboo22`
+//     },
+// ];
+//
+// for (let i = 0; i < users.length; i++) {
+//     const user = users[i];
+//     console.log(users[i].password);
+// }
